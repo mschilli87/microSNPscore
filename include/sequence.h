3bdf3bbb46686188b5429adeb744e6a194aeb68d
Marcel Schilling <marcel.schilling@fu-berlin.de>
2011-07-08 17:09:15 +0200

    Setting standard doxygen descriptions and adding short descriptions to sequence methods

--- include/sequence.h
+++ include/sequence.h
@@ -13,12 +13,46 @@ namespace microSNPscan {
 
 class sequence {
   public:
+    /*****************************************************************//**
+    * @brief standard constructor                                        *
+    *                                                                    *
+    * This is used to create an instance of the class sequence.          *
+    *                                                                    *
+    * @return an empty sequence                                          *
+    *********************************************************************/
+    
     sequence();
 
+    /*****************************************************************//**
+    * @brief destructor                                                  *
+    *                                                                    *
+    * This is used to remove an instance of the sequence class.          *
+    *                                                                    *
+    *********************************************************************/
+    
     ~sequence();
 
+    /*****************************************************************//**
+    * @brief const copy constructor                                      *
+    *                                                                    *
+    * This is used to copy an instance of the sequence class.            *
+    *                                                                    *
+    * @param source sequence object to copy                              *
+    * @return a copy of the source sequence object                       *
+    *********************************************************************/
+    
     sequence(const sequence & source);
 
+    /*****************************************************************//**
+    * @brief assignment operator                                         *
+    *                                                                    *
+    * This is used to copy an instance of  the sequnce class by          *
+    * assigning it to another one                                        *
+    *                                                                    *
+    * @param source the sequece object to be copied                      *
+    * @return a copy of the source sequence object                       *
+    *********************************************************************/
+    
     sequence & operator=(const sequence & source);
 
 };
--- source/sequence.cpp
+++ source/sequence.cpp
@@ -3,14 +3,48 @@
 
 namespace microSNPscan {
 
+/*****************************************************************//**
+* @brief standard constructor                                        *
+*                                                                    *
+* This is used to create an instance of the class sequence.          *
+*                                                                    *
+* @return an empty sequence                                          *
+*********************************************************************/
+
 sequence::sequence() {
 }
+
+/*****************************************************************//**
+* @brief destructor                                                  *
+*                                                                    *
+* This is used to remove an instance of the sequence class.          *
+*                                                                    *
+*********************************************************************/
 
 sequence::~sequence() {
 }
 
+/*****************************************************************//**
+* @brief const copy constructor                                      *
+*                                                                    *
+* This is used to copy an instance of the sequence class.            *
+*                                                                    *
+* @param source sequence object to copy                              *
+* @return a copy of the source sequence object                       *
+*********************************************************************/
+
 sequence::sequence(const sequence & source) {
 }
+
+/*****************************************************************//**
+* @brief assignment operator                                         *
+*                                                                    *
+* This is used to copy an instance of  the sequnce class by          *
+* assigning it to another one                                        *
+*                                                                    *
+* @param source the sequece object to be copied                      *
+* @return a copy of the source sequence object                       *
+*********************************************************************/
 
 sequence & sequence::operator=(const sequence & source) {
 }
