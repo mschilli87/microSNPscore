8e39595b6f9e663cd88f89397020504462cec11e
Marcel Schilling <marcel.schilling@fu-berlin.de>
2011-07-11 12:28:38 +0200

    Adding exon and nucleotide classes

--- include/sequence.h
+++ include/sequence.h
@@ -2,6 +2,9 @@
 #define MICROSNPSCAN_SEQUENCE_H
 
 
+#include <vector>
+using namespace std;
+
 namespace microSNPscan {
 
 /*****************************************************************//**
@@ -54,6 +57,104 @@ class sequence {
     *********************************************************************/
     
     sequence & operator=(const sequence & source);
+
+
+  private:
+    vector<nucleotide> nucleotides;
+
+};
+/*****************************************************************//**
+* @brief exon class
+*
+* This represents an exon as a pair of positions (start/stop).
+*********************************************************************/
+
+class exon {
+  public:
+    /*****************************************************************//**
+    * @brief standard constructor
+    *
+    * This is used to create an instance of the class exon.
+    *
+    * @return an empty exon
+    *********************************************************************/
+    exon();
+
+    /*****************************************************************//**
+    * @brief destructor
+    *
+    * This is used to remove an instance of the exon class.
+    *
+    *********************************************************************/
+    ~exon();
+
+    /*****************************************************************//**
+    * @brief const copy constructor
+    *
+    * This is used to copy an instance of the exon class.
+    *
+    * @param source exon object to copy
+    * @return a copy of the source exon object
+    *********************************************************************/
+    exon(const exon & source);
+
+    /*****************************************************************//**
+    * @brief assignment operator
+    *
+    * This is used to copy an instance of  the exon class by
+    * assigning it to another one
+    *
+    * @param source the exon object to be copied
+    * @return a copy of the source exon object
+    *********************************************************************/
+    exon & operator=(const exon & source);
+
+};
+/*****************************************************************//**
+* @brief nucleotide class
+*
+* This represents a nucleotide.
+*********************************************************************/
+
+class nucleotide {
+  public:
+    /*****************************************************************//**
+    * @brief standard constructor
+    *
+    * This is used to create an instance of the class nucleotide.
+    *
+    * @return an empty exon
+    *********************************************************************/
+    nucleotide();
+
+    /*****************************************************************//**
+    * @brief destructor
+    *
+    * This is used to remove an instance of the nucleotide class.
+    *
+    *********************************************************************/
+    ~nucleotide();
+
+    /*****************************************************************//**
+    * @brief const copy constructor
+    *
+    * This is used to copy an instance of the nucleotide class.
+    *
+    * @param source nucleotide object to copy
+    * @return a copy of the source nucleotide object
+    *********************************************************************/
+    nucleotide(const nucleotide & source);
+
+    /*****************************************************************//**
+    * @brief assignment operator
+    *
+    * This is used to copy an instance of  the nucleotide class by
+    * assigning it to another one
+    *
+    * @param source the nucleotide object to be copied
+    * @return a copy of the source nucleotide object
+    *********************************************************************/
+    nucleotide & operator=(const nucleotide & source);
 
 };
 
--- source/sequence.cpp
+++ source/sequence.cpp
@@ -49,5 +49,89 @@ sequence::sequence(const sequence & source) {
 sequence & sequence::operator=(const sequence & source) {
 }
 
+/*****************************************************************//**
+* @brief standard constructor
+*
+* This is used to create an instance of the class exon.
+*
+* @return an empty exon
+*********************************************************************/
+exon::exon() {
+}
+
+/*****************************************************************//**
+* @brief destructor
+*
+* This is used to remove an instance of the exon class.
+*
+*********************************************************************/
+exon::~exon() {
+}
+
+/*****************************************************************//**
+* @brief const copy constructor
+*
+* This is used to copy an instance of the exon class.
+*
+* @param source exon object to copy
+* @return a copy of the source exon object
+*********************************************************************/
+exon::exon(const exon & source) {
+}
+
+/*****************************************************************//**
+* @brief assignment operator
+*
+* This is used to copy an instance of  the exon class by
+* assigning it to another one
+*
+* @param source the exon object to be copied
+* @return a copy of the source exon object
+*********************************************************************/
+exon & exon::operator=(const exon & source) {
+}
+
+/*****************************************************************//**
+* @brief standard constructor
+*
+* This is used to create an instance of the class nucleotide.
+*
+* @return an empty exon
+*********************************************************************/
+nucleotide::nucleotide() {
+}
+
+/*****************************************************************//**
+* @brief destructor
+*
+* This is used to remove an instance of the nucleotide class.
+*
+*********************************************************************/
+nucleotide::~nucleotide() {
+}
+
+/*****************************************************************//**
+* @brief const copy constructor
+*
+* This is used to copy an instance of the nucleotide class.
+*
+* @param source nucleotide object to copy
+* @return a copy of the source nucleotide object
+*********************************************************************/
+nucleotide::nucleotide(const nucleotide & source) {
+}
+
+/*****************************************************************//**
+* @brief assignment operator
+*
+* This is used to copy an instance of  the nucleotide class by
+* assigning it to another one
+*
+* @param source the nucleotide object to be copied
+* @return a copy of the source nucleotide object
+*********************************************************************/
+nucleotide & nucleotide::operator=(const nucleotide & source) {
+}
+
 
 } // namespace microSNPscan
