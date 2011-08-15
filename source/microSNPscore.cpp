7960b3e72986ef32c02aff80068b9a887772cb68
Marcel Schilling <marcel.schilling@fu-berlin.de>
2011-08-15 10:32:36 +0200

    Changing SNP line regex to correctly group chromosome name -note: input file reading seems to work now

--- source/microSNPscore.cpp
+++ source/microSNPscore.cpp
@@ -43,7 +43,7 @@ void read_SNPs(std::map<SNPID,SNP> & map, filePath path)
     | valid SNP file line stating error in case of failure:    |
      \********************************************************/
     regex_t line_regex;
-    char line_pattern[] = "^([^\t]+)\t([[:alpha:]]+)\t([[:alpha:]]*)\t([^\t]+)\t[+-]\t([[:digit:]]+)$";
+    char line_pattern[] = "^([^\t]+)\t([[:alpha:]]+)\t([[:alpha:]]*)\t([^\t]+)\t([+-])\t([[:digit:]]+)$";
     if(regcomp(&line_regex,line_pattern,REG_EXTENDED) != 0)
     {
       std::cerr << "microSNPscore::read_SNPs\n";
