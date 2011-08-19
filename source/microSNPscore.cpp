28712c7651b24c3750747d0a2c8465a852e0b648
Marcel Schilling <marcel.schilling@fu-berlin.de>
2011-08-19 17:04:24 +0200

    Rearranging main program to keep sequence files as short as possible in memory to lower memory consumption

--- source/microSNPscore.cpp
+++ source/microSNPscore.cpp
@@ -22,6 +22,31 @@ struct prediction
   SNPID              SNP;
 };
 
+void read_sequences(std::map<sequenceID,mRNA> & mRNA_map,filePath mRNA_path,
+                    std::map<sequenceID,miRNA> & miRNA_map,filePath miRNA_path,
+                    const conservationList & conservations)
+{
+     /****************************************************************\ 
+    | Read the given files and insert the corresponing sequences into  |
+    | their maps reusing the sequenceFile object to hold only one file |
+    | at a time in memory:                                             |
+     \****************************************************************/
+    sequenceFile the_file(mRNA_path);
+    the_file.read();
+    for(sequenceFile::const_iterator mRNA_it(the_file.begin());mRNA_it!=the_file.end();++mRNA_it)
+    {
+      mRNA the_mRNA(mRNA_it->get_mRNA(conservations));
+      mRNA_map.insert(std::pair<sequenceID,mRNA>(the_mRNA.get_ID(),the_mRNA));
+    }
+    the_file=sequenceFile(miRNA_path);
+    the_file.read();
+    for(sequenceFile::const_iterator miRNA_it(the_file.begin());miRNA_it!=the_file.end();++miRNA_it)
+    {
+      miRNA the_miRNA(miRNA_it->get_miRNA(conservations));
+      miRNA_map.insert(std::pair<sequenceID,miRNA>(the_miRNA.get_ID(),the_miRNA));
+    }
+}
+
 void read_SNPs(std::map<SNPID,SNP> & map, filePath path)
 {
    /********************************************************\ 
@@ -200,26 +225,13 @@ int main(int argc, char * argv[])
      /***************************\ 
     | Read data from input files: |
      \***************************/
-    sequenceFile mRNA_file(mRNA_file_path);
-    mRNA_file.read();
-    sequenceFile miRNA_file(miRNA_file_path);
-    miRNA_file.read();
     conservationList conservation_list(conservation_file_path);
     std::map<sequenceID,mRNA> mRNAs;
-    for(sequenceFile::const_iterator mRNA_it(mRNA_file.begin());mRNA_it!=mRNA_file.end();++mRNA_it)
-    {
-      mRNA the_mRNA(mRNA_it->get_mRNA(conservation_list));
-      mRNAs.insert(std::pair<sequenceID,mRNA>(the_mRNA.get_ID(),the_mRNA));
-    }
     std::map<sequenceID,miRNA> miRNAs;
-    for(sequenceFile::const_iterator miRNA_it(miRNA_file.begin());miRNA_it!=miRNA_file.end();++miRNA_it)
-    {
-      miRNA the_miRNA(miRNA_it->get_miRNA(conservation_list));
-      miRNAs.insert(std::pair<sequenceID,miRNA>(the_miRNA.get_ID(),the_miRNA));
-    }
     std::map<SNPID,SNP> SNPs;
-    read_SNPs(SNPs,SNP_file_path);
     std::vector<prediction> predictions;
+    read_sequences(mRNAs,mRNA_file_path,miRNAs,miRNA_file_path,conservation_list);
+    read_SNPs(SNPs,SNP_file_path);
     read_predictions(predictions,prediction_file_path);
      /**************************************************\ 
     | Iterate over the predictions printing the original |
